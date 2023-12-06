04841d68c5d4589aee25cd9bc143d5e2dbb35354
SugarCube1337 <danila_pechkurov@mail.ru>
2023-12-06 22:17:53 +0300

    drag-and-drop first sucess, TODO: debug<Entity> and some things

--- app/main.js
+++ app/main.js
@@ -6,54 +6,82 @@
 
 import {h, Component} from 'preact'
 import {Entity, Scene} from 'aframe-react'
+import 'super-hands';
+
 
 const COLORS = ['#D92B6A', '#9564F2', '#FFCF59']
 
 class Main extends Component {
-
     render() {
         return (
-            <Scene
-                environment={{
-                    preset: 'japan',
-                    seed: 1,
-                    skyType: 'gradient',
-                    lightPosition: 'distant',
-                    fog: 0,
-                    ground: 'flat',
-                    groundYScale: 5.0,
-                    groundTexture: 'squares',
-                    groundColor: '#755b5c',
-                    grid: 'dots'
-                }}
-            >
-                <Entity primitive="a-camera" look-controls>
-                    <Entity
-                        primitive="a-cursor"
-                        cursor={{fuse: false}}
-                        material={{color: 'white', shader: 'flat', opacity: 0.75}}
-                        geometry={{radiusInner: 0.005, radiusOuter: 0.007}}
-                        raycaster crawling-cursor
-                    />
-                </Entity>
-                <Entity
-                    primitive="a-octahedron"
-                    detail={2}
-                    radius={2}
-                    position={{ x: 2.5, y: 0.0, z: 0.0 }}
-                    color="#FAFAF1"
-                />
+            <a-scene physics environment={{ preset: 'tron', shadow: true }}>
+                <a-assets>
+                    <a-mixin
+                        id="cube"
+                        geometry="primitive: box; width: 0.33; height: 0.33; depth: 0.33"
+                        hoverable
+                        grabbable
+                        stretchable
+                        draggable
+                        droppable
+                        event-set__hoveron="_event: hover-start; material.opacity: 0.7; transparent: true"
+                        event-set__hoveroff="_event: hover-end; material.opacity: 1; transparent: false"
+                        body="shape: none"
+                        shape="shape: box; halfExtents: 0.165 0.165 0.165"
+                        shadow
+                    ></a-mixin>
+                </a-assets>
 
-            </Scene>
+                <a-entity
+                    camera
+                    look-controls
+                    wasd-controls
+                    position="0 1 1"
+                    capture-mouse
+                    raycaster
+                    cursor="rayOrigin: mouse"
+                    body="type: static; shape: sphere; sphereRadius: 0.001"
+                    super-hands="colliderEvent: raycaster-intersection;
+                     colliderEventProperty: els;
+                     colliderEndEvent: raycaster-intersection-cleared;
+                     colliderEndEventProperty: clearedEls;"
+                >
+                </a-entity>
 
-        )
-    }
+                <a-entity
+                    class="transformer"
+                    position="0 1.6 -1"
+                    color-randomizer
+                    droppable
+                    body="type: static; shape: none"
+                    shape="shape: box; halfExtents: 0.25 0.25 0.25"
+                    geometry="primitive: box; width: 0.5; height: 0.5; depth: 0.5"
+                    event-set__dragon="_event: dragover-start; material.color: orange"
+                    event-set__dragoff="_event: dragover-end; material.color: purple"
+                    material="color: purple"
+                    shadow
+                >
+                    <a-entity text="value: Drag&drop to change color; width: 0.5; wrapCount: 12; align: center" position="0 0 0.25"></a-entity>
+                </a-entity>
 
-    _handleClick() {
-        this.setState({
-            colorIndex: (this.state.colorIndex + 1) % COLORS.length
-        })
-    }
+                <a-entity class="cube" mixin="cube" position="0 0.265 -1" material="color: red"></a-entity>
+                <a-entity class="cube" mixin="cube" position="0 0.265 -0.5" material="color: red"></a-entity>
+
+                {/* Ground collider, предотвращающий падение объектов */}
+                <a-box
+                    body="type: static; shape: none"
+                    shape="shape: box; halfExtents: 50 0.0005 50"
+                    width={100}
+                    height={0.001}
+                    depth={100}
+                    visible={false}
+                ></a-box>
+            </a-scene>
+        );
+    };
+
 }
+
+
 
 export default Main
